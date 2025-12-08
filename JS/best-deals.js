acb1b852b6a3e6fd65a50ccee9f936a08d4b25de
jules474 <hotshoppinginsights@gmail.com>
2025-12-08 17:02:00 +1100

    Update best-deals.js

--- JS/best-deals.js
+++ JS/best-deals.js
@@ -4,8 +4,7 @@ function makeShoppingLink(model) {
   const search = encodeURIComponent(`${model} best price Australia`);
   return `https://www.google.com/search?tbm=shop&q=${search}`;
 }
-
-document.getElementById("x2-link").href = makeShoppingLink("ECOVACS X2 Omni");
-document.getElementById("s8-link").href = makeShoppingLink("Roborock S8 Pro Ultra");
-document.getElementById("l20-link").href = makeShoppingLink("Dreame L20 Ultra");
+document.getElementById('x2-link').href = 'https://www.google.com/search?q=ECOVACS+X2+Omni';
+document.getElementById('s8-link').href = 'https://www.google.com/search?q=Roborock+S8+Pro+Ultra';
+document.getElementById('l20-link').href = 'https://www.google.com/search?q=Dreame+L20+Ultra';
 
