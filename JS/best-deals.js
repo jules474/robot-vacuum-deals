0690dc37823008eaff1d505e900316b2cabe5419
jules474 <hotshoppinginsights@gmail.com>
2025-12-08 16:51:04 +1100

    Update best-deals.js

--- JS/best-deals.js
+++ JS/best-deals.js
@@ -8,6 +8,4 @@ function makeShoppingLink(model) {
 document.getElementById("x2-link").href = makeShoppingLink("ECOVACS X2 Omni");
 document.getElementById("s8-link").href = makeShoppingLink("Roborock S8 Pro Ultra");
 document.getElementById("l20-link").href = makeShoppingLink("Dreame L20 Ultra");
-<script src="main/JS/best-deals.js"></script>
-</body>
-</html>
+
